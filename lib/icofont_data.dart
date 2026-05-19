a059bb8b6447cf592e49ce93cffe81acb3e08b07
gongqinjing <gongqinjing@gmail.com>
2026-05-19 11:15:51 +0800

    Update icofont_data.dart
    fex can't extend

--- lib/icofont_data.dart
+++ lib/icofont_data.dart
@@ -2,11 +2,16 @@ library icofont_flutter;
 
 import 'package:flutter/widgets.dart';
 
-class IcoFontData extends IconData {
-  const IcoFontData(int codePoint)
-      : super(
-          codePoint,
-          fontFamily: 'IcoFont',
-          fontPackage: 'icofont_flutter',
-        );
+class IcoFont {
+  // 私有化构造，禁止创建对象
+  IcoFont._();
+
+  // 静态常量/方法，直接返回 IconData
+  static IconData getIcon(int codePoint) {
+    return IconData(
+      codePoint,
+      fontFamily: 'IcoFont',
+      fontPackage: 'icofont_flutter',
+    );
+  }
 }
