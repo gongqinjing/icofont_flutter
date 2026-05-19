7373cb74f54e0926e44307579bfc5f37a221e418
gongqinjing <gongqinjing@gmail.com>
2026-05-19 11:31:38 +0800

    Update icofont_data.dart
    fix

--- lib/icofont_data.dart
+++ lib/icofont_data.dart
@@ -2,16 +2,14 @@ library icofont_flutter;
 
 import 'package:flutter/widgets.dart';
 
-class IcoFont {
-  // 私有化构造，禁止创建对象
-  IcoFont._();
+class IcoFontData {
+  final int codePoint;
 
-  // 静态常量/方法，直接返回 IconData
-  static IconData getIcon(int codePoint) {
-    return IconData(
-      codePoint,
-      fontFamily: 'IcoFont',
-      fontPackage: 'icofont_flutter',
-    );
-  }
+  const IcoFontData(this.codePoint);
+
+  IconData get icon => IconData(
+        codePoint,
+        fontFamily: 'IcoFont',
+        fontPackage: 'icofont_flutter',
+      );
 }
